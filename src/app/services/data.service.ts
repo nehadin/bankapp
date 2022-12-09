cf0f470c27f82317fe90decce5cfab393e9afc78
testingbetadk <testingbetadk@gmail.com>
2022-12-09 13:45:22 +0530

    initial commmit

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -3,27 +3,32 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class DataService {
-
-  currentuser=' '
-  constructor() { }
-
-  userDetails: any = {
-    1001: { acno: 1001, username: "anju", password: 1231, balance: 0 },
-    1002: { acno: 1002, username: "amal", password: 1232, balance: 0 },
-    1003: { acno: 1003, username: "arun", password: 1233, balance: 0 },
-    1004: { acno: 1004, username: "mega", password: 1234, balance: 0 }
-  }
 
   //all the logic related with this database will be created and partially declared here , like the methods/function
   //and along will declare arguments and its type, alwys the type has to be mentioned along with the variable
+
+
+export class DataService {
+
+  currentuser=' '
+  currentacno=' '
+
+  constructor() { }
+
+  userDetails: any = {
+    1001: { acno: 1001, username: "anju", password: 1231, balance: 0 ,transaction:[]},
+    1002: { acno: 1002, username: "amal", password: 1232, balance: 0,transaction:[] },
+    1003: { acno: 1003, username: "arun", password: 1233, balance: 0,transaction:[] },
+    1004: { acno: 1004, username: "mega", password: 1234, balance: 0 ,transaction:[]}
+  }
+
 
   register(acno: any, uname: any, psw: any) {
     var userDetails = this.userDetails
     if (acno in userDetails) {
       return false
     } else {
-      userDetails[acno] = { acno, username: uname, password: psw, balance: 0 }
+      userDetails[acno] = { acno, username: uname, password: psw, balance: 0 ,transaction:[]}
       return true
     }
   }
@@ -32,7 +37,9 @@ export class DataService {
     var userDetails = this.userDetails
     if (acno in userDetails) {
       if (psw == userDetails[acno]["password"]) {
-        //Store name
+        //storing account number
+        this.currentacno=acno
+         //Store name
         this.currentuser=userDetails[acno]['username']
         return true
       } else {
@@ -49,6 +56,7 @@ export class DataService {
     if (acno in userDetails) {
       if (password == userDetails[acno]["password"]) {
         userDetails[acno]["balance"] += amnt
+        userDetails[acno]['transaction'].push({type:'CREDIT',amount:amnt})
         return userDetails[acno]["balance"]
       } else {
         return false
@@ -65,6 +73,7 @@ export class DataService {
       if (password == userDetails[acno]["password"]) {
         if (wit <= userDetails[acno]["balance"]) {
           userDetails[acno]["balance"] -= wit
+          userDetails[acno]["transaction"].push({type:'DEBIT',amount:wit})
           return userDetails[acno]["balance"]
         } else {
           alert('Insufficient balance')
@@ -78,6 +87,10 @@ export class DataService {
       alert("Incorrect Ac. No.")
       return false
     }
+  }
+
+  gettransaction(acno:any){
+    return this.userDetails[acno]["transaction"]
   }
 }
 
